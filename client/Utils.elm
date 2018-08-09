94d5f72deef266385858b5446a6ac8e9c45e96d9
Tom Morton <tomm@riseup.net>
2018-08-09 20:21:14 +0100

    Fixed money formatting

--- client/Utils.elm
+++ client/Utils.elm
@@ -4,6 +4,8 @@ import Time
 import Date
 import String
 import List
+import FormatNumber
+import FormatNumber.Locales exposing (Locale, usLocale)
 
 timeEqYYMMDDHHMM : Time.Time -> Time.Time -> Bool
 timeEqYYMMDDHHMM t1 t2 =
@@ -46,21 +48,9 @@ timeFormatShort t =
             (Date.minute d |> toString)
     in Date.fromTime t |> dateFormatShort
 
+moneyLocale : Locale
+moneyLocale = { usLocale | decimals = 0 }
+
 moneyFormat : Int -> String
 moneyFormat m =
-    -- wow, this is shit. what's wrong with me?
-    let bits num = List.reverse <| String.split "" (toString num)
-        formatByParts l = 
-            let len = List.length l
-            in if len == 0 then ""
-                else if len < 3 then
-                    String.append ((formatByParts <| List.drop 3 l)) (String.join "" <| List.reverse <| List.take 3 l)
-                else 
-                    String.append ((formatByParts <| List.drop 3 l) ++ ",") (String.join "" <| List.reverse <| List.take 3 l)
-    in
-        if m >= 1000 then
-            "€" ++ formatByParts (bits (m // 1000)) ++ "k"
-
-
-        else
-            "€" ++ formatByParts (bits m)
+    "€" ++ (FormatNumber.format moneyLocale <| toFloat (m//1000)) ++ "k"
