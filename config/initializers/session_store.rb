343bfabf97478839b11538c19219db7f2d9b4838
Tom Morton <tomm@riseup.net>
2018-08-11 19:53:07 +0100

    Expire session cookies after 1 day

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_fitba_server_session', expire_after: 30.days
+# expire after 1 day so that app reload is forced, and people get new features ;)
+Rails.application.config.session_store :cookie_store, key: '_fitba_server_session', expire_after: 1.days
